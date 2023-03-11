55b8729fdad33d8b7c97dca4b4e3edaf69239916
Emil <emilmirzoiev@gmail.com>
2023-03-11 10:14:58 +0100

    QuestionViewController + QuestionView

---
+++ RabbleWabble/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  RabbleWabble
+//
+//  Created by Emil on 10.03.23.
+//
+
+import UIKit
+
+public class QuestionView: UIView {
+    
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel: UILabel!
+    @IBOutlet public var incorrectCountLabel: UILabel!
+    @IBOutlet public var promptLabel: UILabel!
+    @IBOutlet public var hintLabel: UILabel!
+    
+}
