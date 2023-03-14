ad0036e2e0e02235f79992afe3bad3f1255616a7
Emil <emilmirzoiev@gmail.com>
2023-03-14 12:27:32 +0100

    SequentialQuestionStrategy implements BaseQuestionStrategy

--- RabbleWabble/Strategies/RandomQuestionStrategy.swift
+++ RabbleWabble/Strategies/RandomQuestionStrategy.swift
@@ -13,7 +13,7 @@ public class RandomQuestionStrategy: BaseQuestionStrategy {
     public convenience init(questionGroupCaretaker: QuestionGroupCaretaker) {
         let questionGroup = questionGroupCaretaker.selectedQuestionGroup!
         let randomSource = GKRandomSource.sharedRandom()
-        let questions = randomSource.arrayByShufflingObjects( in: questionGroup.questions) as! [Question]
+        let questions = randomSource.arrayByShufflingObjects(in: questionGroup.questions) as! [Question]
         self.init(questionGroupCaretaker: questionGroupCaretaker, questions: questions)
     }
 }
